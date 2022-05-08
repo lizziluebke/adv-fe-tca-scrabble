ecf2497fa128009096ca220aa5c662098b895a69
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-05-08 18:43:01 -0500

    github pages build

---
+++ docs/static/js/3107.b0a84763.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3107.b0a84763.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunktca_scrabble=self.webpackChunktca_scrabble||[]).push([[3107],{3107:function(c,e,a){a.r(e),a.d(e,{g:function(){return n}});var n=a(7923).i}}]);
+//# sourceMappingURL=3107.b0a84763.chunk.js.map
---
+++ docs/static/js/3127.e7c2e138.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3127.e7c2e138.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunktca_scrabble=self.webpackChunktca_scrabble||[]).push([[3127],{3127:function(e,r,t){t.r(r),t.d(r,{i:function(){return n}});var n=function(e){return e&&""!==e.dir?"rtl"===e.dir.toLowerCase():"rtl"===(null===document||void 0===document?void 0:document.dir.toLowerCase())}}}]);
+//# sourceMappingURL=3127.e7c2e138.chunk.js.map
